141925f97ad4e025fb34b5517127e6a2906b5862
Jordan Wellington Lint <eyeoh3@Juanma.local>
2015-02-26 17:51:52 +0100

    Add content

--- includes/random.php
+++ includes/random.php
@@ -1,48 +1,59 @@
 <?php
+    
+    $who = array(
+        'interesting people',
+        'a multi-displinary team'
+    );
 
-    /* */
+    $how = array(
+        'craft',
+        'develop',
+        'make',
+        'produce'
+    );
+
+    $what = array(
+        'clear',
+        'effective',
+        'striking',
+        'memorable',
+        'notable',
+        'powerful',
+        'remarkable'
+    );
+
+    $whatelse = array(
+        'creative',
+        'genuine',
+        'imaginative',
+        'individual',
+        'ingenious',
+        'innovative',
+        'insightful',
+        'unconventional',
+        'unique'
+    );
     
     $city = array(
+        // Top 100 Most Populous Cities In Africa 2010
+        // http://www.blatantworld.com/feature/africa/most_populous_cities.html
+        'Lagos', 'Kinshasa', 'Cairo', 'Ibadan', 'Alexandria', 'Abidjan', 'Cape Town', 'Durban', 'Gizeh', 'Addia Ababa', 'Casablanca', 'Nairobi', 'Dar es Salaam', 'Luanda', 'Dakar', 'Umm Durman', 'Accra', 'Khartoum', 'Benin', 'Kano', 'Algiers', 'Douala', 'Port Harcourt', 'Kaduna', 'Johannesburg', 'Conakry', 'Kumasi', 'Rabat', 'Yaounde', 'Soweto', 'Bamako', 'Harare', 'Pretoria', 'Antananarivo', 'Lubumbashi', 'Lome', 'Aba', 'Muqdisho', 'Mbuji-Mayi', 'Kampala', 'Lusaka', 'Ouagadougou', 'Abuja', 'Brazzaville', 'Port Elizabeth', 'Maputo', 'Maiduguri', 'Ilorin', 'Shubra-El-Khema', 'Monrovia'
         
-        // 50 Largest Cities in Africa
-        // http://theafricaneconomist.com/50-largest-cities-in-africa/#.VO2_r1PF8Yc
-        'Lagos', 'Cairo', 'Kinshasa', 'Alexandria', 'Casablanca', 'Ivory Coast', 'Kano', 'Ibadan', 'Cape Town', 'Addis Ababa', 'Giza', 'Nairobi', 'Dar es Salaam', 'Dakar', 'Durban', 'Luanda', 'Tripoli', 'Harare', 'Algiers', 'Omdurman', 'Johannesburg', 'Accra', 'Rabat', 'Conakry', 'Kaduna', 'Khartoum', 'Douala', 'Pretoria', 'Soweto', 'Lusaka', 'Mogadishu', 'Brazzaville', 'Yaoundé', 'Maputo', 'Lubumbashi', 'Port Harcourt', 'Benin', 'Freetown', 'Subra al-Haymah', 'Maiduguri', 'Kampala', 'Bulawayo', 'Fez', 'Bamako', 'Mbuji-May', 'Zaria', 'Antananarivo', 'Ouagadougou', 'Port Elizabeth', 'Kolwezi',
-    
         // Top 100 Most Populous Cities In Asia 2010
         // http://www.blatantworld.com/feature/asia/most_populous_cities.html
         /* 'Shanghai', 'Bombay', 'Karachi', 'Delhi', 'Istanbul', 'Manila', 'Dhaka', 'Soeul', 'Tokyo', 'Jakarta', 'Tehran', 'Peking', 'Lahore', 'Bangkok', 'Singapore', 'Bangalore', 'Baghdad', 'Calcutta', 'Rangoon', 'Riyadh', 'Chongqing', 'Madras', 'Xian', 'Wuhan', 'Ankara', 'Hyderabad', 'Ahmadabad', 'Chengdu', 'Ho Chi Minh', 'Chattagam', 'Yokohama', 'Tianjin', 'Shenyang', 'Pune', 'Harbin', 'Nanjing', 'Surat', 'Pusan', 'Pyongyang', 'Jiddah', 'Kanpur', 'Jaipur', 'Guangzhou', 'Mosul', 'Faisalabad', 'Izmir', 'Changchun', 'Taiyuan', 'Lakhnau', 'Taipei' */
+        
+        // Top 100 Most Populous Cities In Oceania 2010
+        // http://www.blatantworld.com/feature/oceania/most_populous_cities.html
+        /*
+        'Sydney', 'Melbourne', 'Brisbane', 'Makasar', 'Perth', 'Adelaide', 'Manado', 'Gold Coast-Tweed Heads', 'Auckland', 'Ambon', 'Manukau', 'Christchurch', 'Honolulu', 'Kupang', 'Palu', 'Canberra-Queanbeyan', 'Mataram', 'Kendari', 'Port Moresby', 'Central Coast', 'Newcastle', 'North Shore', 'Wollongong', 'Waitakere', 'Sunshine', 'Wellington', 'Dili', 'Hamilton', 'Bitung', 'Gorontalo', 'Jaya Pura', 'Sorong', 'Geelong', 'Townsville', 'Hobart', 'Pare Pare', 'Tauranga', 'Mobagu', 'Dunedin', 'Tanete', 'Ternate', 'Sombaopu', 'Cairns', 'Nouméa', 'Toowoomba', 'Palopo', 'Lower Hutt', 'Lae', 'Bau Bau', 'Pallangga' */
     
     );
-
-    $a = rand(0, count($city)-1); $selectedCity = "$city[$a]";
-
-    /* */
     
-    $weare = array(
-        'a multi-displinary <mark>team</mark>',
-        'an independent <mark>studio</mark>'
-    );
-
-    $b = rand(0, count($weare)-1); $selectedWeare = "$weare[$b]";
-
-    /* */
+    $a = rand(0, count($who)-1); $selectedWho = "$who[$a]";
+    $b = rand(0, count($how)-1); $selectedHow = "$how[$b]";
+    $c = rand(0, count($what)-1); $selectedWhat = "$what[$c]";
+    $d = rand(0, count($whatelse)-1); $selectedWhatelse = "$whatelse[$d]";
+    $z = rand(0, count($city)-1); $selectedCity = "$city[$z]";
     
-    $doing = array(
-        'building',
-        'crafting',
-        'making',
-        'producing'
-    );
-
-    $c = rand(0, count($doing)-1); $selectedDoing = "$doing[$c]";
-
-    /* */
-    
-    $what = array(
-        'design',
-        'work'
-    );
-
-    $d = rand(0, count($what)-1); $selectedWhat = "$what[$d]";
-
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-<?php include 'includes/random.php'; ?>
+<?php /* It began in Africa...
+
+                     .c.
+                   .d$$e$.
+                .e$P"   ^*%
+             .e$P"
+           z$$$"
+        z$$$$$F
+     .d$$$$$$$F
+  .d$$$$$$$$$$F
+.3$$$$$$$$$$$$F
+  $$$$$" ""$*$"
+   P $"    $ $
+  d d"     $ $
+  $ *      $ $
+   ) '     $ $
+    % '    $ $
+     % "   $ $
+      - )  * *
+
+*/
+
+include 'includes/random.php';
+
+$graphic = 'Fine Arts,<br>Brand Identity,<br>Editorial Design,<br>UX/UI Design, and<br>all sorts of<br>Visual Communication.';
+$digital = 'Creative Coding,<br>Web Design & Development<br>and, CMS Implementation<br>powered by<br>Progressive enhancement<br>and Open Standards.';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -23,18 +50,35 @@
                         <div class="header-main">
                             <hgroup>
                                 <h1>i/o studio</h1>
-                                <h2 class="lead">We are <?php echo $selectedWeare.' '.$selectedDoing; ?> visual identities and digital solutions through effective and innovative <?php echo $selectedWhat; ?>. Based in in <del><?php echo $selectedCity; ?></del> Barcelona. <a data-toggle="collapse" href="#colophon" aria-expanded="false" aria-controls="colophon">Read more →</a></h2>
+                                <h2 class="lead">We are <?php echo $selectedWho; ?> working together to <?php echo $selectedHow; ?> <abbr class="initialism" data-toggle="tooltip" data-html="true" title="<?php echo $graphic; ?>">graphic</abbr> and <abbr class="initialism" data-toggle="tooltip" data-html="true" title="<?php echo $digital; ?>">digital</abbr> solutions through <?php echo $selectedWhat; ?> and <?php echo $selectedWhatelse; ?> <mark>design</mark>, based in <del><?php echo $selectedCity; ?></del> Barcelona. <a data-toggle="collapse" href="#read-more" aria-expanded="false" aria-controls="read-more">Read More →</a></h2>
                             </hgroup>
                         </div>
                     </header><!-- #masthead -->
                 </div><!-- .col-md-12 -->
             </div><!-- .row -->
-            <div class="row collapse" id="colophon">
+            <div class="row collapse" id="read-more">
                 <div class="col-md-12">
                     <div>
+                        <h2>What's i/o studio?</h2>
                         <p>Fruitcake powder tiramisu I love. Pastry halvah cookie oat cake dragée muffin pastry candy canes. I love I love ice cream icing fruitcake I love soufflé candy.</p>
+                        <h2>Who are we?</h2>
                         <p>Candy ice cream fruitcake lemon drops I love muffin toffee tart. Caramels apple pie marshmallow caramels. Pie cookie jelly topping tootsie roll danish. Cupcake wafer I love chocolate bar jelly beans gummies chocolate cake.</p>
+                        <h2>How it's built with.</h2>
                         <p>Tiramisu sweet jelly sesame snaps halvah. Cheesecake donut candy canes gingerbread croissant bear claw I love bear claw. Bear claw cookie carrot cake biscuit pastry cake croissant.</p>
+                        <h2>Open Standards and Accessibility Declaration</h2>
+                        <p>Cupcake ipsum dolor. Sit amet topping. Sweet roll cookie powder topping chocolate bar oat cake bonbon chocolate soufflé. Pudding brownie apple pie jelly-o pudding carrot cake.</p>
+                        <h2>Grahics</h2>
+                        <p>Icing croissant I love topping sweet roll gummi bears sweet sugar plum donut. Jelly beans soufflé chupa chups tootsie roll cupcake cake jelly beans. Carrot cake apple pie macaroon sesame snaps apple pie cheesecake cake icing I love.</p>
+                        <h2>Pictures</h2>
+                        <p>I love I love candy canes liquorice bear claw I love biscuit ice cream. Wafer liquorice soufflé dragée I love sweet roll dragée sweet roll. Marzipan cookie cheesecake I love.</p>
+                        <h2>License</h2>
+                        <p>Bonbon I love oat cake caramels jelly-o. I love carrot cake macaroon halvah sesame snaps gummies powder jelly-o. Fruitcake muffin I love candy canes. Macaroon bear claw chupa chups tiramisu muffin cake croissant gummi bears I love.</p>
+                        <h2>One last thing</h2>
+                        <p><img class="img-responsive" src="https://d13yacurqjgara.cloudfront.net/users/187497/screenshots/1933823/dinos-walking01.gif"></p>
+                        <blockquote>
+                            <p>Embrace change or become a Design-O-Saur.</p>
+                            <footer>Von Glitschka</footer>
+                        </blockquote>
                     </div><!-- #colophon -->
                 </div><!-- .col-md-12 -->
             </div><!-- .row -->
@@ -46,7 +90,7 @@
                                 <div id="content" class="site-content" role="main">
                                     <div class="col-md-6">
                                         <h2 id="work">Work</h2>
-                                        <p>Donut topping candy gingerbread donut I love. Lemon drops fruitcake cupcake. Wafer jelly I love caramels. Tootsie roll pudding brownie pie macaroon gingerbread. Jelly-o tiramisu oat cake caramels pudding cupcake. Cookie gummi bears chupa chups. I love chupa chups brownie cotton candy. Danish danish cupcake gummies gummi bears.</p>
+                                        <p>We plan, design and build websites and interfaces, across all digital platforms, for projects ranging from interaction design and creative coding to information architecture or data aesthetics.</p>
                                     </div><!-- .col-md-6 -->
                                     <div class="col-md-6">
                                         <h2>Contact</h2>
@@ -65,7 +109,7 @@
                             <strong>i/o studio</strong>
                             <p>7th Floor, <a href="https://goo.gl/maps/x5j9b" target="_blank" data-toggle="tooltip" title="View map">313 Trav. de les Corts, Barcelona 08029</a>.<br>
                                 (+34) 934 103 368 / (+34) 618 261 598<br></p>
-                             <p><a href="#" target="_blank">Bēhance</a>, <a href="#" target="_blank">Tumblr</a>.</p>
+                             <p><a href="#" target="_blank">Bēhance</a>, <a href="http://eyeohstudio.tumblr.com/" target="_blank">Tumblr</a>.</p>
                         </address><!-- address -->
                         <div class="site-info">
                             <p>i/o studio (cc) <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">Some Rights Reserved</a>.</p>
