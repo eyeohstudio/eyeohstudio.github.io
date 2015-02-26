951561af48dbb04bc82e2ce0bb7f76eef770dd29
Jordan Wellington Lint <eyeoh3@Juanma.local>
2015-02-26 17:51:44 +0100

    Adjust styles

--- scripts/default.js
+++ scripts/default.js
@@ -1,9 +1,20 @@
 jQuery(document).ready(function($) {
-
+    
+    //
 	$('body').fadeIn('slow');
     
+    //
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
-    })
-
+    });
+    
+    //
+    $("a[href='#read-more']").click(function() {
+        if ($(this).text() == 'Read More →') {
+            $(this).text('Read Less ←');
+        } else {
+            $(this).text('Read More →');
+        }
+    });
+    
 });
