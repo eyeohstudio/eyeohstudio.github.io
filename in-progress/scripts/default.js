463dedb4930a776c7141393e8f1aa74ea49d4942
Jordan Wellington Lint <eyeoh3@Juanma.local>
2015-03-05 14:22:44 +0100

    Translate random() to jQuery
    So it’s compatible with GitHub Pages!

--- in-progress/scripts/default.js
+++ in-progress/scripts/default.js
@@ -17,4 +17,61 @@ jQuery(document).ready(function($) {
         }
     });
     
+    // randomWho
+    var who = new Array(
+        'a multi-displinary team',
+        'interesting people'
+    ),
+    randomWho = who[Math.floor( Math.random() * who.length )];
+    $('.randomWho').text( randomWho );
+    
+    // randomHow
+    var how = new Array(
+        'craft',
+        'develop',
+        'make',
+        'produce'
+    ),
+    randomHow = how[Math.floor( Math.random() * how.length )];
+    $('.randomHow').text( randomHow );
+    
+    // randomWhat
+    var what = new Array(
+        'effective',
+        'powerful',
+        'remarkable',
+        'striking'
+    ),
+    randomWhat = what[Math.floor( Math.random() * what.length )];
+    $('.randomWhat').text( randomWhat );
+    
+    // randomWhatelse
+    var whatelse = new Array(
+        'creative',
+        'imaginative',
+        'innovative',
+        'unconventional'
+    ),
+    randomWhatelse = whatelse[Math.floor( Math.random() * whatelse.length )];
+    $('.randomWhatelse').text( randomWhatelse );
+    
+    // randomCity
+    var city = new Array(
+        'Arkham',
+        'Bikini Bottom',
+        'Coruscant',
+        'Emerald City',
+        'Gotham City',
+        'Hundred Acre Wood',
+        'King\'s Landing',
+        'Metropolis',
+        'Neo Tokyo',
+        'New New York',
+        'Raccoon City',
+        'San Fransokyo',
+        'Twin Peaks'
+    ),
+    randomCity = city[Math.floor( Math.random() * city.length )];
+    $('.randomCity').text( randomCity );
+    
 });
